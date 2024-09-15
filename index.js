91307a122721a636350595dc40d0f0e71be8d3e2
Lucas Lins <lucaslins.link@gmail.com>
2024-09-15 01:19:56 -0300

    Cadastrar meta ok

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-const {select} = require('@inquirer/prompts')
+const {select, input} = require('@inquirer/prompts')
 
-const start = async() => {
+let meta = {
+    value: 'Enviar ao menos um commit para o github',
+    checked: false
+    }
+
+
+let metas = [meta]
+
+const cadastrarMeta = async () => {
+    console.clear();
+    const meta = await input({
+        message: "Digite uma meta: "
+    })
+
+    if(meta.length == 0){
+        console.log("A meta precisa ter um título.")
+        return
+    }
+
+    metas.push(
+        {value: meta, checked: false}
+    )
+    
+}
+
+const listarMetas = async () => {
+    console.clear();
+    console.log(metas);
+}
+
+const start = async() => { //Função assíncrona
     while (true) {
 
-        const option = await select({
+        const option = await select({ 
             message: "Menu >",
             choices: [
                 {
-                    name: "Cadastrar meta",
+                    name: "Cadastrar metas",
                     value: "cadastrar" //O valor escolhido será entrada para option
                 },
                 {
-                    name: "Listar meta",
+                    name: "Listar metas",
                     value: "listar"
                 },
                 {
@@ -24,10 +54,12 @@ const start = async() => {
 
         switch(option) {
             case "cadastrar":
-                console.log("Cadastro")
+                console.log("Selecionou Cadastro")
+                await cadastrarMeta();
                 break
             case "listar":
-                console.log("Listagem")
+                console.log("Selecionou Listagem")
+                await listarMetas();
                 break
             case "sair":
                 console.log("Sair do programa")
